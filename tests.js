79d455b2fd1d5313732235a044dea6a81bbda31a
Michael Carroll <carrollm2@gmail.com>
2017-11-09 19:39:15 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -4,10 +4,7 @@ if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
-    console.log("Expected: 70, Received: "=+index.height)
 }
-
-
 
 if (index.height === 70) {
     console.log("The height is correct")
